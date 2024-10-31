5221377743aacfb00bcca2eddc06044f4b0935ec
Mophe <87334468+Mopheshi@users.noreply.github.com>
2024-10-31 10:20:40 +0100

    Accuracy: 80.00%

---
+++ Practice Projects/Pima Indians Diabetes/main1.py
@@ -0,0 +1,51 @@
+from tkinter.font import names
+
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from imblearn.over_sampling import SMOTE
+
+from main import FILE_PATH, RANDOM_STATE, TEST_SIZE
+
+column_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI',
+                'DiabetesPedigreeFunction', 'Age', 'Outcome']
+
+data = pd.read_csv(FILE_PATH, header=None, names=column_names)
+
+data.fillna(data.median(), inplace=True)
+data.infer_objects(copy=False)
+
+X = data.drop('Outcome', axis=1)
+y = data['Outcome']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Handle imbalanced data
+smote = SMOTE()
+X_resampled, y_resampled = smote.fit_resample(X_scaled, y)
+
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=TEST_SIZE,
+                                                    random_state=RANDOM_STATE)
+
+model = RandomForestClassifier()
+
+# Hyperparameter tuning
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+grid_search.fit(X_train, y_train)
+
+best_model = grid_search.best_estimator_
+
+y_pred = best_model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy * 100:.2f}%')
